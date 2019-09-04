5ac1f0085bde246dabd52124d68254155b4f2502
Priyal Sobti <priyalsobti@gmail.com>
2019-09-04 23:30:44 +0530

    changes up till makeit.js

--- public/assets/time_table.js
+++ public/assets/time_table.js
@@ -1,10 +1,15 @@
 $(function() {
   $('#addbutton').on('click', function() {
-    alert('heelo');
+    var count = $('#sno').val();
     var sti = $("#inpTimestart").val();
     var eti = $('#inpTimeend').val();
     var desci = $('#inpDesc').val();
+    if (!$("#inpTimestart").val() || !$('#inpTimeend').val() || !$('#inpDesc').val()) {
+      alert("Enter value");
+      return;
+    }
     var newDet = {
+      tid: count,
       st: sti,
       et: eti,
       desc: desci
@@ -14,11 +19,16 @@ $(function() {
       url: '/timeTable',
       data: newDet,
       success: function(data) { //here the updated data is used after the data has been changed using the /todo app controller
-        //do something with the data via front-end framework
         location.reload();
       }
     });
 
 
   });
+
+  $('.clickIcon').click(function(ev) {
+    var txt = $(this).parent().text();
+
+    // console.log($(this).parent().text().toJSON());
+  })
 });
