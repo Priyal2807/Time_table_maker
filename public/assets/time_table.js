a7d7325a1230720fa3a2cd27c21a560ab3f9f6d5
Priyal Sobti <priyalsobti@gmail.com>
2019-09-07 22:47:33 +0530

    deleting and updating span elements done

--- public/assets/time_table.js
+++ public/assets/time_table.js
@@ -1,4 +1,5 @@
 $(function() {
+
   $('#addbutton').on('click', function() {
     var count = $('#sno').val();
     var sti = $("#inpTimestart").val();
@@ -28,6 +29,22 @@ $(function() {
 
   $('.clickIcon').click(function(ev) {
     var txt = $(this).parent().text();
+    txt = JSON.stringify(txt);
+    var f = txt.charCodeAt(1);
+    f = f - 48;
+    if (txt.charAt(2) >= '0' && txt.charAt(2) <= '9') {
+      f = f * 10;
+      var s = txt.charCodeAt(2) - 48;
+      f = f + s;
+    }
+    alert("You sure you want to delete the entire item!");
+    $.ajax({
+      type: 'DELETE',
+      url: '/timeTable/' + f,
+      success: function(data) {
+        location.reload();
+      }
+    });
 
     // console.log($(this).parent().text().toJSON());
   })
